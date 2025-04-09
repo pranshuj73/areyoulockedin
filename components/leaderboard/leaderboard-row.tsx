0bdcda9cf70c97b512c3f04a02c8ad166fc0f44a
voltycodes <153023205+voltycodes@users.noreply.github.com>
2025-04-09 23:48:02 +0530

    feat: add profile link on leaderboard

--- components/leaderboard/leaderboard-row.tsx
+++ components/leaderboard/leaderboard-row.tsx
@@ -4,6 +4,7 @@ import {
 } from "@/components/ui/table"
 import { MedalIcon, TrophyIcon } from "lucide-react"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import Link from "next/link"
 
 interface LeaderboardRowProps {
   position: number
@@ -25,19 +26,19 @@ const LanguagePills = ({ languages }: { languages: string[] }) => (
 )
 
 const User = ({username, pfp}: {username: string, pfp?: string}) => (
-  <div className="flex items-center gap-2">
+  <Link className="flex items-center gap-2 hover:bg-foreground/10 w-min pl-1 pr-3 py-1 -my-1 rounded-full transition-all duration-300 ease-out" href={`https://x.com/${username}`} target="_blank">
     <Avatar>
       <AvatarImage src={pfp} />
       <AvatarFallback>{username.at(0)}</AvatarFallback>
     </Avatar>
     <span>@{username}</span>
-  </div>
+  </Link>
 )
 
 export default function LeaderboardRow({ position, username, time, languages, pfp }: LeaderboardRowProps) {
   return (
     <TableRow>
-      <TableCell className="font-medium text-center flex items-center justify-center gap-2">
+      <TableCell className="font-medium text-center flex items-center justify-center gap-2 py-6">
         {position === 1 ? <TrophyIcon className="size-5 -ml-7" /> : position === 2 || position === 3 ? <MedalIcon className="size-5 -ml-7" /> : null}
         #{position}
       </TableCell>
