5cd02e0ae34e69283e8f040413222a38465fb4a6
voltycodes <153023205+voltycodes@users.noreply.github.com>
2025-04-10 14:00:49 +0530

    feat: extend support for more languages
    newly supported languages:
    odin
    nim
    julia
    lisp
    gleam
    solidity
    webassembly
    zig
    cobol
    elm

--- lib/extmap/map.ts
+++ lib/extmap/map.ts
@@ -1,68 +1,83 @@
 import type { FileType } from "@/lib/extmap/lang";
 
 export const fileExtensionMap: Record<string, FileType> = {
-  // Programming Languages
-  "js": "JavaScript",
-  "mjs": "JavaScript", // ES Modules
-  "cjs": "JavaScript", // CommonJS Modules
-  "jsx": "JavaScript", // JavaScript React Syntax
-  "ts": "TypeScript",
-  "mts": "TypeScript", // ES Modules
-  "cts": "TypeScript", // CommonJS Modules
-  "tsx": "TypeScript", // TypeScript React Syntax
-  "py": "Python",
-  "pyw": "Python",     // Python for Windows GUI (no console)
-  "go": "Go",
-  "rs": "Rust",
-  "java": "Java",
-  "jsp": "Java",       // Java Server Pages (Often contains Java code)
-  "kt": "Kotlin",
-  "kts": "Kotlin",     // Kotlin Script
-  "swift": "Swift",
-  "m": "ObjectiveC",   // Objective-C
-  "mm": "ObjectiveC",  // Objective-C++
-  "cs": "CSharp",      // C#
-  "fs": "FSharp",      // F#
-  "fsx": "FSharp",     // F# Script
+  // Programming Languages - Sorted roughly alphabetically by language name for grouping
+  "asm": "Assembly",   // Assembly language
+  "s": "Assembly",     // Assembly language (Unix)
+  "bat": "Batch",      // Windows Batch
+  "cmd": "Batch",      // Windows Batch (newer)
   "c": "C",
-  "h": "C",            // C Header (often used with C++ too, but primary C)
+  "h": "C",            // C Header (often used with C/C++ too, but primary C)
   "cpp": "CPlusPlus",  // C++
   "cxx": "CPlusPlus",  // C++
   "cc": "CPlusPlus",   // C++
   "hpp": "CPlusPlus",  // C++ Header
   "hxx": "CPlusPlus",  // C++ Header
   "hh": "CPlusPlus",   // C++ Header (less common)
-  "php": "PHP",
-  "phtml": "PHP",      // PHP/HTML mixed
-  "rb": "Ruby",
-  "pl": "Perl",
-  "pm": "Perl",        // Perl Module
-  "lua": "Lua",
-  "scala": "Scala",
-  "sc": "Scala",       // Scala Script
-  "groovy": "Groovy",
-  "gvy": "Groovy",
-  "gy": "Groovy",
-  "dart": "Dart",
-  "ex": "Elixir",
-  "exs": "Elixir",     // Elixir Script
-  "erl": "Erlang",
-  "hrl": "Erlang",     // Erlang Header
+  "cs": "CSharp",      // C#
   "clj": "Clojure",
   "cljs": "Clojure",   // ClojureScript
   "cljc": "Clojure",   // Clojure/ClojureScript portable
+  "cob": "COBOL",      // COBOL Source
+  "cbl": "COBOL",      // COBOL Source
+  "cpy": "COBOL",      // COBOL Copybook
+  "dart": "Dart",
+  "ex": "Elixir",
+  "exs": "Elixir",     // Elixir Script
+  "elm": "Elm",
+  "erl": "Erlang",
+  "hrl": "Erlang",     // Erlang Header
+  "fs": "FSharp",      // F#
+  "fsx": "FSharp",     // F# Script
+  "gleam": "Gleam",
+  "go": "Go",
+  "groovy": "Groovy",
+  "gvy": "Groovy",
+  "gy": "Groovy",
   "hs": "Haskell",
   "lhs": "Haskell",    // Literate Haskell
+  "java": "Java",
+  "jsp": "Java",       // Java Server Pages (Often contains Java code)
+  "js": "JavaScript",
+  "mjs": "JavaScript", // ES Modules
+  "cjs": "JavaScript", // CommonJS Modules
+  "jsx": "JavaScript", // JavaScript React Syntax
+  "jl": "Julia",
+  "kt": "Kotlin",
+  "kts": "Kotlin",     // Kotlin Script
+  "lisp": "Lisp",      // Common Lisp
+  "lsp": "Lisp",       // Common Lisp / AutoLISP
+  "cl": "Lisp",        // Common Lisp
+  "lua": "Lua",
+  "nim": "Nim",
+  "nims": "Nim",       // Nim Script
+  "m": "ObjectiveC",   // Objective-C
+  "mm": "ObjectiveC",  // Objective-C++
+  "odin": "Odin",
+  "pl": "Perl",
+  "pm": "Perl",        // Perl Module
+  "php": "PHP",
+  "phtml": "PHP",      // PHP/HTML mixed
+  "ps1": "PowerShell", // PowerShell Script
+  "py": "Python",
+  "pyw": "Python",     // Python for Windows GUI (no console)
   "r": "R",
-  "sql": "SQL",
+  "rb": "Ruby",
+  "rs": "Rust",
+  "scala": "Scala",
+  "sc": "Scala",       // Scala Script
   "sh": "Shell",       // Generic Shell
   "bash": "Shell",     // Bash Script
   "zsh": "Shell",      // Zsh Script
-  "ps1": "PowerShell", // PowerShell Script
-  "bat": "Batch",      // Windows Batch
-  "cmd": "Batch",      // Windows Batch (newer)
-  "asm": "Assembly",   // Assembly language
-  "s": "Assembly",     // Assembly language (Unix)
+  "sol": "Solidity",   // Solidity (Ethereum)
+  "sql": "SQL",
+  "swift": "Swift",
+  "ts": "TypeScript",
+  "mts": "TypeScript", // ES Modules
+  "cts": "TypeScript", // CommonJS Modules
+  "tsx": "TypeScript", // TypeScript React Syntax
+  "wat": "WebAssembly", // WebAssembly Text Format
+  "zig": "Zig",
 
   // Markup, Styling, Data, Config & Other Formats
   "html": "HTML",
@@ -95,4 +110,3 @@ export const fileExtensionMap: Record<string, FileType> = {
   "graphql": "GraphQL",  // GraphQL Schema Definition Language or queries
   "gql": "GraphQL",
 };
-
