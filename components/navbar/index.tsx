fab21548887404af74698eca5400aa16159ac31d
voltycodes <153023205+voltycodes@users.noreply.github.com>
2025-05-04 10:05:10 +0530

    fix nav h1 size

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 h-18 w-full backdrop-blur-2xl border-b-2 border-dashed z-50">
       <div className="flex justify-between items-center p-4 set-mw-center h-full">
-        <h1 className="font-mono text-lg font-semibold">areyoulocked.in</h1>
+        <h1 className="font-mono font-semibold">areyoulocked.in</h1>
         <div className="flex items-center justify-center gap-2">
           <SignedOut>
             <Button variant="outline" asChild>
