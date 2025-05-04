dc6748531588d95ceef8f8fe2cd86ea346c09ede
voltycodes <153023205+voltycodes@users.noreply.github.com>
2025-05-04 10:54:27 +0530

    fix: nav & footer content alignment

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -2,8 +2,8 @@ import { DetailModal } from "../details";
 
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 text-sm h-18 w-full bg-background border-t-2 border-dashed z-50">
-      <div className="flex justify-between items-center p-4 set-mw-center h-full">
+    <footer className="fixed bottom-0 left-0 text-sm h-18 w-full p-8 bg-background border-t-2 border-dashed z-50">
+      <div className="flex justify-between items-center set-mw-center h-full">
         <DetailModal />
         <p>areyoulockedin © {new Date().getFullYear()}</p>
       </div>
--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -9,8 +9,8 @@ import { Button } from '../ui/button';
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 h-18 w-full backdrop-blur-2xl border-b-2 border-dashed z-50">
-      <div className="flex justify-between items-center p-4 set-mw-center h-full">
+    <nav className="fixed top-0 left-0 h-18 w-full p-8 backdrop-blur-2xl border-b-2 border-dashed z-50">
+      <div className="flex justify-between items-center set-mw-center h-full">
         <h1 className="font-mono font-semibold">areyoulocked.in</h1>
         <div className="flex items-center justify-center gap-2">
           <SignedOut>
