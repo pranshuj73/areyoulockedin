8b9753596dbe38ff5adce573c1fa5cdc2bfd4f40
voltycodes <153023205+voltycodes@users.noreply.github.com>
2025-05-18 13:30:47 +0530

    update readme popup

--- components/details/content/help-pay-for-our-servers.tsx
+++ components/details/content/help-pay-for-our-servers.tsx
@@ -5,8 +5,9 @@ import Image from "next/image";
 export default function HelpPayForOurServers() {
   return (
     <DetailsCard title="Help pay for our servers!" description="so that we can keep buying coffee and pulling all nighters.">
-      <p>We're a couple of broke college kids building this project and neon.tech isnt so generous with their free plan 😭.</p>
-      <p>If you like the project &amp; can spare some change, please help us out lol. <Link className="text-blue-700 dark:text-blue-300" href="https://buymeacoffee.com/mittalsatyansh" target="_blank">Here's our Buy Me A Coffee link.</Link></p>
+      <p>We’re broke college kids duct-taping this project together. Your support = more uptime, fewer tears.</p>
+      <p>Enjoying the project? Consider dropping us some change (and pleaseeee drop your X username so we can shout you out in the credits).</p>
+      <Link className="text-blue-700 dark:text-blue-300" href="https://buymeacoffee.com/mittalsatyansh" target="_blank">Here's our Buy Me A Coffee link.</Link>
       <Image src="/broke.jpg" alt="Beggar Emoji" width={200} height={200} className="mt-4" />
     </DetailsCard>
   )
